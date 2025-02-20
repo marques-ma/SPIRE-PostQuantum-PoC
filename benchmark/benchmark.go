92767c6e663522496d94c0fc81ab4ea97ca53c94
Marco Antonio Marques <70598892+marques-ma@users.noreply.github.com>
2025-02-20 14:24:20 -0300

    Update benchmark.go

--- benchmark/benchmark.go
+++ benchmark/benchmark.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"os"
+        "fmt"
+        "os"
 
-	oqsopenssl "github.com/marques-ma/pq-openssl-3.x"
+        "github.com/marques-ma/oqsopenssl"
 )
 
 func main() {
 
-	// Validate command-line arguments
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: benchmark <algorithm> <output_file>")
-		os.Exit(1)
-	}
+        // Validate command-line arguments
+        if len(os.Args) < 4 {
+                fmt.Println("Usage: benchmark <algorithm> <duration_in_seconds> <output_file>")
+                os.Exit(1)
+        }
 
-	algorithm := os.Args[1]  
-	outputFile := os.Args[2] 
+        algorithm := os.Args[1]
+        duration := os.Args[2]
+        outputFile := os.Args[3]
 
-	// Start the container
-	err := oqsopenssl.StartOQSContainer()
-	if err != nil {
-		fmt.Println("Error starting OQS container:", err)
-		return
-	}
-	defer oqsopenssl.StopOQSContainer()
+        // Start the container
+        err := oqsopenssl.StartOQSContainer()
+        if err != nil {
+                fmt.Println("Error starting OQS container:", err)
+                return
+        }
+        defer oqsopenssl.StopOQSContainer()
 
-	// Run the benchmark
-	err = oqsopenssl.BenchmarkAlgorithm(algorithm, outputFile, 30) // Duration is hardcoded to 10 seconds
-	if err != nil {
-		fmt.Println("Error benchmarking algorithm:", err)
-		return
-	}
+        // Run the benchmark
+        err = oqsopenssl.BenchmarkAlgorithm(algorithm, outputFile, duration)
+        if err != nil {
+                fmt.Println("Error benchmarking algorithm:", err)
+                return
+        }
 
-	fmt.Printf("Benchmarking completed for algorithm %s. Results saved to %s\n", algorithm, outputFile)
+        fmt.Printf("Benchmarking completed for algorithm %s. Results saved to %s\n", algorithm, outputFile)
 
 }
